2dcf79baaf9a7eea62b20476c3d17db08fafbea9
Yug-soni <yugsoni8946842393@gmail.com>
2021-07-11 16:18:29 +0530

    added admin-user, category, sub-category and product table with some of the basic data

--- base-scripts-ecom-test.sql
+++ base-scripts-ecom-test.sql
@@ -1,1 +1,48 @@
+create table admin_user
+(
+    `key`    varchar(10) not null,
+    password varchar(10) not null
+);
+
+create table category
+(
+    category    varchar(30) not null,
+    category_id int auto_increment,
+    constraint category_category_id_uindex
+        unique (category_id),
+    constraint category_category_uindex
+        unique (category)
+);
+
+alter table category
+    add primary key (category_id);
+
+create table product
+(
+    product_id    int auto_increment,
+    serial_number varchar(14)  not null,
+    product_name  varchar(100) not null,
+    display_name  varchar(50)  not null,
+    constraint product_product_id_uindex
+        unique (product_id),
+    constraint product_serial_number_uindex
+        unique (serial_number)
+);
+
+alter table product
+    add primary key (product_id);
+
+create table sub_category
+(
+    sub_category_id int auto_increment,
+    sub_category    int not null,
+    constraint sub_category_sub_category_id_uindex
+        unique (sub_category_id),
+    constraint sub_category_sub_category_uindex
+        unique (sub_category)
+);
+
+alter table sub_category
+    add primary key (sub_category_id);
+
 
